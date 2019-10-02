7a5a012d1acf3adb9984463fd4cf056939988261
Naveenkumar Mortha <29162085+nmortha@users.noreply.github.com>
2019-10-02 07:02:40 +0530

    modified the code
    added shebang

--- src/sum/sum.py
+++ src/sum/sum.py
@@ -1,5 +1,6 @@
-num1=(input('Enter number1 to calculate sum of two munmbers: '))
-num2=(input('Enter number2 to calculate sum of two munmbers: '))
+#!/usr/bin/env python
+num1=(input('Enter number1 to calculated sum of two munmbers: '))
+num2=(input('Enter number2 to calculated sum of two munmbers: '))
 print ('Enterted Numbers are : '+ num1 + " ,"+ num2)
 total=int(num1)+int(num2)
 print('Sum of 2 numbers '+ num1+ ','+ num2 + ' is :', total)
