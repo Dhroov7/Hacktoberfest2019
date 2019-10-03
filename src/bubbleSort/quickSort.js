a100dbd9b27fc3b0c314a44736eb9c04f6699582
geraldsamosir <gerald.unforgiven@gmail.com>
2019-10-03 15:41:51 +0700

    buble sort

---
+++ src/bubbleSort/quickSort.js
@@ -0,0 +1,14 @@
+
+const  bubbleSort =(arr)=>{
+    var len = arr.length;
+    for (var i = len-1; i>=0; i--){
+      for(var j = 1; j<=i; j++){
+        if(arr[j-1]>arr[j]){
+            var temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+         }
+      }
+    }
+    return arr;
+ };
