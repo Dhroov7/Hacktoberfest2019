ae791c83259b696dab5862f3579c0cc4a5e88f16
luisdeol <luisfelipekai@gmail.com>
2019-10-03 08:00:24 -0300

    Luis Felipe - HelloWorld C#

---
+++ src/helloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System; 
+
+namespace HelloWorldForHacktober { 
+    class Program { 
+        static void Main(string[] args) { 
+
+            Console.WriteLine("Hello World!"); 
+
+            Console.ReadKey(); 
+        } 
+    } 
+} 
