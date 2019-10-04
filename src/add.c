79ecc79b4b9cb539f4fb0e942d05b2a4924b0f74
vimal kumar <vimal.btech.cs18@iiitranchi.ac.in>
2019-10-04 11:30:13 +0530

    vimal

---
+++ src/add.c
@@ -0,0 +1,15 @@
+    #include<stdio.h>
+     
+    int main()
+    {
+       int a, b, c;
+       
+       printf("Enter two numbers to add\n");
+       scanf("%d%d", &a, &b);
+       
+       c = a + b;
+       
+       printf("Sum of numbers = %d\n", c);
+       printf("welcome in 2020");
+       return 0;
+    }
