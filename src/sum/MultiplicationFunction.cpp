ebe3ba9fc5b69c0d0a56d3d443b6708d6f7d8d67
David Aponte <alexdavidve@gmail.com>
2019-10-06 20:13:32 -0400

    David Aponte

---
+++ src/sum/MultiplicationFunction.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int MultiplicationFunction(int a, int b)
+{
+    return a*b;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b ;
+    cout << MultiplicationFunction(a, b) << endl ;
+    return 0;
+}
