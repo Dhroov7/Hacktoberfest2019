fd434c88959cde11f0caf914f26ce6a69bde1423
Tejas Kashinath <42380254+tejaskashinathofficial@users.noreply.github.com>
2019-10-08 15:48:59 +0530

    Update HelloWorld.cs

--- src/helloWorld/HelloWorld.cs
+++ src/helloWorld/HelloWorld.cs
@@ -2,7 +2,7 @@ using System;
 namespace HelloWorldApp { 
       
     // Class declaration 
-    class Geeks { 
+    class Hacktober { 
           
         // Main Method 
         static void Main(string[] args) { 
