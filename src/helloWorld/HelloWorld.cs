701a5a8f3bd95a2c31dcf59926893d1afe92faf8
Tejas Kashinath <42380254+tejaskashinathofficial@users.noreply.github.com>
2019-10-08 15:48:15 +0530

    Added HelloWorld in C#

---
+++ src/helloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System; 
+namespace HelloWorldApp { 
+      
+    // Class declaration 
+    class Geeks { 
+          
+        // Main Method 
+        static void Main(string[] args) { 
+              
+            // statement 
+            // printing Hello World! 
+            Console.WriteLine("Hello World!"); 
+              
+            // To prevents the screen from  
+            // running and closing quickly 
+            Console.ReadKey(); 
+        } 
+    } 
+}
