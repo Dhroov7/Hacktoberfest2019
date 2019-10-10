18e04f02ae698c34b11acedcc6a625ba7f652073
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-10-10 18:14:29 +0530

    add ashmit

---
+++ upload_files_php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload Files</title>
+</head>
+<body>
+<h1>Upload Files</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file"><br>
+        <button type="submit" name="submit">Upload</button>
+    </form>
+</body>
+</html>
