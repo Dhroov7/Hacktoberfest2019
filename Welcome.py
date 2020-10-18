1231b79c55b338e93d8727e841ea3f7337ccba36
Shubham Kumar Sharma <53513799+shubhamkumarcs@users.noreply.github.com>
2020-10-18 10:35:19 +0530

    Hacktoberfest info

---
+++ Welcome.py
@@ -0,0 +1,3 @@
+print("Welcome to the Hacktoberfest 2020")
+Print("Hacktoberfest® is open to everyone in our global community. 
+Whether you’re a developer, student learning to code, event host, or company of any size, you can help drive growth of open source and make positive contributions to an ever-growing community. All backgrounds and skill levels are encouraged to complete the challenge.")
