42880d206ffbed42cdf0bec30adf96e6cf238f6f
D4AG0N <38225598+D4AG0N@users.noreply.github.com>
2020-10-21 15:45:44 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
 print('Hello world, welcome to Hacktoberfest 2019')
+print('Hello world, welcome to Hacktoberfest 2020')
 #this is a hello world program written in python programming language.
 #I added a message for the event
